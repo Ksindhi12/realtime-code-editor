26d5f45523cde39ce7ad525b31744a6104c028cb
Krish metronome <21ucs204@lnmiit.ac.in>
2024-07-09 00:11:14 +0530

    render config

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const path = require('path');
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-});
+const io = new Server(server);
 
 app.use(express.static('build'));
 app.use((req, res, next) => {
@@ -77,6 +72,5 @@ io.on('connection', (socket) => {
 });
 
 
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
